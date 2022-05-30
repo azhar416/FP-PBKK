c31f3c6b7cdcc91ec4ebb2ac88e4f4e8a82e29d4
RavindraWiguna <77233945+RavindraWiguna@users.noreply.github.com>
2022-05-31 05:19:06 +0700

    Add localization for register | Ganti bahasa masih manual

---
+++ resources/lang/en/register.php
@@ -0,0 +1,12 @@
+<?php
+
+// resources/lang/en/login.php
+
+return [
+	"name" => "Name",
+	"email" => "Email",
+	"password" => "Password",
+	"confirm_password" => "Confirm Password",
+	"registered" => "Already registered?",
+	"button" => "REGISTER",
+];
---
+++ resources/lang/id/register.php
@@ -0,0 +1,12 @@
+<?php
+
+// resources/lang/id/login.php
+
+return [
+	"name" => "Nama",
+	"email" => "Email",
+	"password" => "Kata Sandi",
+	"confirm_password" => "Konfirmasi Kata Sandi",
+	"registered" => "Sudah terdaftar?",
+	"button" => "DAFTAR"
+];
