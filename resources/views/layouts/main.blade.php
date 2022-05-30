03319a6ada37cfb200ad571431aa432dee5262bc
azhar416 <rar.azhar.416@gmail.com>
2022-05-30 15:39:54 +0700

    update

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <title>RBTC</title>
+
+  <!-- Fonts -->
+  <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+  <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+  <script src="{{ asset('js/app.js') }}" defer></script>
+
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  
+
+  <style>
+    body {
+      font-family: 'Nunito', sans-serif;
+    }
+  </style>
+</head>
+
+<body class="bg-gray-200">
+  {{-- <x-partial.navbar /> --}}
+
+  {{ $slot }}
+
+  {{-- <x-partial.footer /> --}}
+
+  <script src="https://unpkg.com/feather-icons"></script>
+
+  <script>
+    feather.replace();
+  </script>
+
+</body>
+
+</html>
